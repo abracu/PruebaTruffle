055ec23aa8efe9edd425259907823cf46aebb6ac
Alfredo Bravo Cuero <yo@alfredobravocuero.co>
2021-05-28 23:35:35 -0500

    Codificacion final Truffle React

---
+++ test/ItemManagerTest.js
@@ -0,0 +1,25 @@
+const ItemManager = artifacts.require("./ItemManager.sol");
+contract("ItemManager", accounts => {
+    let itemManagerInstance;
+    let itemName;
+    let itemPrice;
+    let result;
+
+    beforeEach(async () => {
+        itemManagerInstance = await ItemManager.deployed();
+        itemName = "test1";
+        itemPrice = 500;
+        result = await itemManagerInstance.createItem(itemName, itemPrice,{from: accounts[0]});
+    });
+
+    describe("test ItemManager", function(){
+        it("Create new item", async () => {
+            assert.equal(result.logs[0].args._itemIndex, 0, "There should be one item index in there");
+        });
+
+        it("Name de item same as passed", async () => {
+            const item = await itemManagerInstance.items(0);
+            assert.equal(item._identifier, itemName, "The item has a different identifier");
+        });
+    });
+});
